43c8946ce9856f4eae30cc3e9cbb2d1a373acc30
mtn002 <m.caruso007@studenti.unibs.it>
2022-06-11 09:58:52 +0200

    Aggiunta libreria

--- src/PgAr2022/Esame/CarusoMartina/Menu.java
+++ src/PgAr2022/Esame/CarusoMartina/Menu.java
@@ -1,4 +1,13 @@
 package PgAr2022.Esame.CarusoMartina;
+import java.util.*;
 
 public class Menu {
+    private Personaggio p;
+    private Battaglia scontro;
+
+    public void menu(){
+
+
+    }
+
 }
